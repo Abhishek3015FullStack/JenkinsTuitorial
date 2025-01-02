0326d7464e3a66c5c8a663561cb48e474a788cdb
JSGITAbhishek <136972656+Abhishek3015FullStack@users.noreply.github.com>
2025-01-02 15:10:34 +0530

    w
    wr

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { NewComponent } from './new/new.component';
 import { New1Component } from './new1/new1.component';
 import { New3Component } from './new3/new3.component';
+import { New4Component } from './new4/new4.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NewComponent,
     New1Component,
-    New3Component
+    New3Component,
+    New4Component
   ],
   imports: [
     BrowserModule,
