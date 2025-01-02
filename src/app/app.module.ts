15d5816eb5e87faca9f74819332968fb61d435b7
JSGITAbhishek <136972656+Abhishek3015FullStack@users.noreply.github.com>
2025-01-02 15:07:05 +0530

    fg
    dfgd

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NewComponent } from './new/new.component';
 import { New1Component } from './new1/new1.component';
+import { New3Component } from './new3/new3.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NewComponent,
-    New1Component
+    New1Component,
+    New3Component
   ],
   imports: [
     BrowserModule,
